acb5154d0a3d2eff3b3ebed1f8fa06054ef40473
dudemaiaf <dudu_maia13@hotmail.com>
2017-07-06 01:53:56 -0400

    Add files via upload

---
+++ Boss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    private Transform target;
+    private Animator animator;
+    public int vidaBoss;
+    private bool podeAtacar;
+    public float vel;
+    Rigidbody2D rb;
+    private bool podeReceber;
+    
+
+    void Awake()
+    {
+        transform.tag = "Enemy"; //isto irá por a tag Enemy
+    }
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform; //player
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        podeAtacar = true;
+        podeReceber = true;
+        animator = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Animator>();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = Vector2.left * vel;
+
+
+    }
+}
